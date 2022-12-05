54a6bc68b6d5cc22ab9f02c1f46d88c5ebce97ec
Romeo <romeo.moreaux35@gmail.com>
2022-12-05 20:06:04 +0100

    add type picture and display them + add region picture

--- src/components/PokemonDescription.vue
+++ src/components/PokemonDescription.vue
@@ -1,70 +1,95 @@
 <template>
-<div class="detail" v-if="desc">
-    <div class="box box_img">
-        <div>
-            <ul class="img_list">
-                <li>        
-                    <figure class="image is-128x128">
-                        <img :src="desc.sprites.back_default" alt="Placeholder image" height="50" width="50">
-                    </figure>
-                </li>
-                <li>        
-                    <figure class="image is-128x128">
-                        <img :src="desc.sprites.front_default" alt="Placeholder image" height="50" width="50">
-                    </figure>
-                </li>
-                <li>        
-                    <figure class="image is-128x128">
-                        <img :src="desc.sprites.back_shiny" alt="Placeholder image" height="50" width="50">
-                    </figure>
-                </li>
-                <li>        
-                    <figure class="image is-128x128">
-                        <img :src="desc.sprites.front_shiny" alt="Placeholder image" height="50" width="50">
-                    </figure>
-                </li>
-            </ul>
+    <div class="description" v-if="desc">
+        <div class="box box_img">
+            <div>
+                <ul class="img_list">
+                    <li>        
+                        <figure class="image is-128x128">
+                            <img :src="desc.sprites.back_default" alt="Placeholder image" height="50" width="50">
+                        </figure>
+                    </li>
+                    <li>        
+                        <figure class="image is-128x128">
+                            <img :src="desc.sprites.front_default" alt="Placeholder image" height="50" width="50">
+                        </figure>
+                    </li>
+                    <li>        
+                        <figure class="image is-128x128">
+                            <img :src="desc.sprites.back_shiny" alt="Placeholder image" height="50" width="50">
+                        </figure>
+                    </li>
+                    <li>        
+                        <figure class="image is-128x128">
+                            <img :src="desc.sprites.front_shiny" alt="Placeholder image" height="50" width="50">
+                        </figure>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="box box_informations">
+            <h1 class="pokemon_name">{{desc.name}}</h1>
+            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus, diam non venenatis malesuada, eros magna sodales orci, et bibendum arcu nisi sed sem. Phasellus nec tincidunt lacus. Nulla consequat justo viverra egestas semper. Vestibulum vestibulum, odio et eleifend iaculis, ante arcu ullamcorper dolor, id tincidunt est massa sit amet metus. Sed gravida, mauris sed placerat pretium, ipsum lorem pulvinar est, eget rutrum felis ante a velit. Nam sed magna metus. Sed tincidunt porta eros. Etiam gravida turpis dolor, in tristique est elementum eget. Cras posuere facilisis augue, at viverra augue. </div>
+            <div class="box_detail">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th v-for="stat in desc.stats" :key="stat.stat.name">{{stat.stat.name}}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td v-for="base_stat in desc.stats" :key="base_stat">{{base_stat.base_stat}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <ul class="pokemon_types">
+                    <li v-for="t in desc.types" :key="t.type.name"><figure class="image is-128x128"><img :src="require(`@/assets/${t.type.name}.png`)"/></figure></li>
+                </ul>
+            </div>
         </div>
     </div>
-    <div class="box box_informations">
-        <h1>{{desc.name}}</h1>
-        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus, diam non venenatis malesuada, eros magna sodales orci, et bibendum arcu nisi sed sem. Phasellus nec tincidunt lacus. Nulla consequat justo viverra egestas semper. Vestibulum vestibulum, odio et eleifend iaculis, ante arcu ullamcorper dolor, id tincidunt est massa sit amet metus. Sed gravida, mauris sed placerat pretium, ipsum lorem pulvinar est, eget rutrum felis ante a velit. Nam sed magna metus. Sed tincidunt porta eros. Etiam gravida turpis dolor, in tristique est elementum eget. Cras posuere facilisis augue, at viverra augue. </div>
-        <div>
-
-        </div>
-    </div>
-</div>
 </template>
 
 <script>
 
 import PokeApiService from '@/services/PokeApiService';
 export default{
-props: ['id'],
-data(){
-    return {
-        desc: null
-    }
-},
-created(){
-        PokeApiService.getPokemonDescription(this.id)
-        .then(response => {      
-            this.desc = response.data
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    }
+    props: ['id'],
+    data(){
+        return {
+            desc: null
+        }
+    },
+    created(){
+            PokeApiService.getPokemonDescription(this.id)
+            .then(response => {      
+                this.desc = response.data
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        },
 }
 
 </script>
 
 <style>
 
-.detail{
+.description{
     display: flex;
     margin: 100px;
     
+}
+
+.box_detail{
+    display: flex;
+    flex-direction: column;
+}
+
+.pokemon_types{
+    display: flex;
+    justify-content: center;
+    margin: 10px;
 }
 
 .box_img{
@@ -73,8 +98,15 @@ created(){
     margin: 0rem !important;
 }
 
-.box_informations{
+.table{
+    margin: 50px;
+}
 
+.pokemon_name{
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 200%;
+    margin: 10px;
 }
 
 </style>
