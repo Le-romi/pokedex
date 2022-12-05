97f91f9c8e675a889e7f3d7d3dc0e019bb6043ce
Romeo <romeo.moreaux35@gmail.com>
2022-12-05 21:49:50 +0100

    add img region

--- src/components/PokemonDescription.vue
+++ src/components/PokemonDescription.vue
@@ -1,55 +1,46 @@
 <template>
     <div class="description" v-if="desc">
-        <div class="box box_img">
-            <div>
-                <ul class="img_list">
-                    <li>        
-                        <figure class="image is-128x128">
-                            <img :src="desc.sprites.back_default" alt="Placeholder image" height="50" width="50">
-                        </figure>
-                    </li>
-                    <li>        
-                        <figure class="image is-128x128">
-                            <img :src="desc.sprites.front_default" alt="Placeholder image" height="50" width="50">
-                        </figure>
-                    </li>
-                    <li>        
-                        <figure class="image is-128x128">
-                            <img :src="desc.sprites.back_shiny" alt="Placeholder image" height="50" width="50">
-                        </figure>
-                    </li>
-                    <li>        
-                        <figure class="image is-128x128">
-                            <img :src="desc.sprites.front_shiny" alt="Placeholder image" height="50" width="50">
-                        </figure>
-                    </li>
-                </ul>
-            </div>
-        </div>
         <div class="box box_informations">
-            <h1 class="pokemon_name">{{desc.name}}</h1>
+            <div class="pokemon_title">       
+                <figure class="image is-128x128">
+                        <img :src="desc.sprites.back_default" alt="Placeholder image" height="50" width="50">
+                </figure>     
+                <figure class="image is-128x128">
+                    <img :src="desc.sprites.front_default" alt="Placeholder image" height="50" width="50">
+                </figure>
+                <h1 class="pokemon_name">{{desc.name}}</h1>      
+                <figure class="image is-128x128">
+                        <img :src="desc.sprites.back_shiny" alt="Placeholder image" height="50" width="50">
+                </figure>
+    
+                <figure class="image is-128x128">
+                    <img :src="desc.sprites.front_shiny" alt="Placeholder image" height="50" width="50">
+                </figure>    
+            </div>      
             <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempus, diam non venenatis malesuada, eros magna sodales orci, et bibendum arcu nisi sed sem. Phasellus nec tincidunt lacus. Nulla consequat justo viverra egestas semper. Vestibulum vestibulum, odio et eleifend iaculis, ante arcu ullamcorper dolor, id tincidunt est massa sit amet metus. Sed gravida, mauris sed placerat pretium, ipsum lorem pulvinar est, eget rutrum felis ante a velit. Nam sed magna metus. Sed tincidunt porta eros. Etiam gravida turpis dolor, in tristique est elementum eget. Cras posuere facilisis augue, at viverra augue. </div>
-            <div class="box_detail">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th v-for="stat in desc.stats" :key="stat.stat.name">{{stat.stat.name}}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td v-for="base_stat in desc.stats" :key="base_stat">{{base_stat.base_stat}}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <ul class="pokemon_types">
-                    <li v-for="t in desc.types" :key="t.type.name"><figure class="image is-128x128"><img :src="require(`@/assets/${t.type.name}.png`)"/></figure></li>
-                </ul>
+            <div class="pokemon_data">
+                <div class="box_detail">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th v-for="stat in desc.stats" :key="stat.stat.name">{{stat.stat.name}}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td v-for="base_stat in desc.stats" :key="base_stat">{{base_stat.base_stat}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <ul class="pokemon_types">
+                        <li v-for="t in desc.types" :key="t.type.name"><figure class="image is-128x128"><img :src="require(`@/assets/types/${t.type.name}.png`)"/></figure></li>
+                    </ul>
+                </div>
+                <img class="has-ratio" width="800" height="400" src="@/assets/regions/kanto.png"/>
             </div>
         </div>
     </div>
 </template>
-
 <script>
 
 import PokeApiService from '@/services/PokeApiService';
@@ -69,6 +60,17 @@ export default{
                 console.log(error)
             })
         },
+        getRegion(id){
+            if(id >= 0  && id <= 151){
+                return require(`@/assets/regions/kanto.png`);
+            } else if(id >= 152  && id <= 251){
+                return "@/assets/regions/johto.png";
+            } else if(id >= 252  && id <= 386){
+                return "@/assets/regions/hoenn.png";
+            } else {
+                return "@/assets/regions/sinnoh.png";
+            }
+        }
 }
 
 </script>
@@ -86,16 +88,22 @@ export default{
     flex-direction: column;
 }
 
+.pokemon_title{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.pokemon_data{
+    display: flex;
+    justify-content: space-around;
+    margin: 50px;
+}
+
 .pokemon_types{
     display: flex;
     justify-content: center;
     margin: 10px;
-}
-
-.box_img{
-    width: 200px;
-    text-align: center;
-    margin: 0rem !important;
 }
 
 .table{
